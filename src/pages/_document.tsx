2325399e09f49dfd9d3b16a134c8d8ff291d7ac0
Yasmin <yasminsdcastro@gmail.com>
2023-03-02 15:44:17 -0300

    feat: favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+        <link rel="icon" href="/icons/favicon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
