c8ccd250838572ff52e3ef86840e70c679ecbeb8
Yasmin <yasminsdcastro@gmail.com>
2023-03-03 10:23:48 -0300

    refactor: icon color

--- src/components/Pages/Dashboard/TopBar/index.tsx
+++ src/components/Pages/Dashboard/TopBar/index.tsx
@@ -2,6 +2,7 @@ import { Container, Wrapper } from "./styles";
 import { Input } from "@mantine/core";
 import { BsSearch } from "react-icons/bs";
 import { IoLogOutOutline } from "react-icons/io5";
+import { colors } from "@/styles/GlobalStyles";
 
 const RightSection: React.FC = () => {
   return (
@@ -13,7 +14,7 @@ const RightSection: React.FC = () => {
             console.log("SAIR");
           }}
         >
-          <IoLogOutOutline size={36} />
+          <IoLogOutOutline color={colors.grey50} size={36} />
         </button>
       </Container>
     </Wrapper>
