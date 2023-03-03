d915cca6865f6e09babe99cc7430de9c1c6481ba
Yasmin <yasminsdcastro@gmail.com>
2023-03-03 15:47:59 -0300

    server side

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,7 +3,7 @@ import SEO from "@/components/Global/SEO";
 import TopBar from "@/components/Pages/Dashboard/TopBar";
 import { PageContainer, PageWrapper } from "@/styles/Page";
 import SolutionsPanel from "@/components/Pages/Dashboard/SolutionsPanel";
-import { GetStaticProps, NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import prisma from "@/lib/prisma";
 import { Software } from "@prisma/client";
 
@@ -29,7 +29,7 @@ const Dashboard: NextPage<DasboardInterface> = ({ softwares }) => {
 
 export default Dashboard;
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   const softwares = await prisma.software.findMany();
 
   return {
