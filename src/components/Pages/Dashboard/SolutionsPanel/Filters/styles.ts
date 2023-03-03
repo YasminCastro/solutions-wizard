2d6f964dc54fdec26b67c14bd44a856c29262b80
Yasmin <yasminsdcastro@gmail.com>
2023-03-03 13:35:55 -0300

    feat: tags

--- src/components/Pages/Dashboard/SolutionsPanel/Filters/styles.ts
+++ src/components/Pages/Dashboard/SolutionsPanel/Filters/styles.ts
@@ -29,3 +29,56 @@ export const Solution = styled.div`
     color: ${colors.grey50};
   }
 `;
+
+export const Categories = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .categories-tags {
+    display: flex;
+    gap: 4px;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: 12px;
+  }
+
+  button {
+    color: ${colors.purple1000};
+    background: ${colors.purple100};
+  }
+
+  .mantine-Button-root:hover {
+    background: ${colors.purple300};
+  }
+
+  button:hover {
+    background: ${colors.purple300};
+  }
+`;
+
+export const TagsButtons = styled.div`
+  display: flex;
+  gap: 4px;
+  justify-content: center;
+
+  align-items: center;
+
+  border-bottom: 1px solid ${colors.lightGrey};
+
+  button {
+    color: ${colors.black};
+    background: transparent;
+
+    font-weight: 200;
+    font-size: 14px;
+  }
+
+  .mantine-Button-root:hover {
+    background: ${colors.lightGrey};
+  }
+
+  button:hover {
+    background: ${colors.lightGrey};
+  }
+`;
