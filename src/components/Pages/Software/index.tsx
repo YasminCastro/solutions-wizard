11e7f49ecdd8a1decf2d15d92ff64a68158e9cf5
Yasmin <yasminsdcastro@gmail.com>
2023-03-06 10:55:35 -0300

    feat: reducer

--- src/components/Pages/Software/index.tsx
+++ src/components/Pages/Software/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import axios from "axios";
 import moment from "moment";
 import { useForm } from "@mantine/form";
@@ -6,13 +6,17 @@ import { Button, Table, TextInput } from "@mantine/core";
 import { FaTrash } from "react-icons/fa";
 
 import { useSoftwares } from "@/providers/softwares";
+import { INITIAL_STATE_SOFTWARES, softwareReducer } from "./softwareRecucer";
+
 import { Container, ContentWrapper, TableContainer, Wrapper } from "./styles";
 import { colors } from "@/styles/GlobalStyles";
 
 const SoftwareForm: React.FC = () => {
   const { softwares, setRefreshSoftwares, setSoftwares } = useSoftwares();
-  const [createSoftwareLoading, setCreateSoftwareLoading] = useState(false);
-  const [deleteSoftwareLoading, setDeleteSoftwareLoading] = useState(false);
+  const [state, dispatch] = useReducer(
+    softwareReducer,
+    INITIAL_STATE_SOFTWARES
+  );
 
   const form = useForm({
     initialValues: {
@@ -33,7 +37,7 @@ const SoftwareForm: React.FC = () => {
         <Button
           variant="subtle"
           onClick={() => handleDelete(element.id)}
-          disabled={deleteSoftwareLoading}
+          disabled={state.deleteSoftwareLoading}
           sx={{ "&[data-disabled]": { pointerEvents: "all" } }}
           compact
         >
@@ -44,7 +48,8 @@ const SoftwareForm: React.FC = () => {
   ));
 
   const handleSubmit = async (values: { name: string }) => {
-    setCreateSoftwareLoading(true);
+    dispatch({ type: "SET_SUBMIT_LOADING", payload: true });
+
     try {
       await axios.post("/api/softwares/create", {
         name: values.name,
@@ -53,12 +58,13 @@ const SoftwareForm: React.FC = () => {
     } catch (error) {
       console.log(error);
     } finally {
-      setCreateSoftwareLoading(false);
+      dispatch({ type: "SET_SUBMIT_LOADING", payload: false });
     }
   };
 
   const handleDelete = async (id: number) => {
-    setDeleteSoftwareLoading(true);
+    dispatch({ type: "SET_DELETE_LOADING", payload: true });
+
     try {
       const index = softwares.findIndex((x) => x.id === id);
       softwares.splice(index, 1);
@@ -70,7 +76,7 @@ const SoftwareForm: React.FC = () => {
     } catch (error) {
       console.log(error);
     } finally {
-      setDeleteSoftwareLoading(false);
+      dispatch({ type: "SET_DELETE_LOADING", payload: false });
     }
   };
 
@@ -87,7 +93,7 @@ const SoftwareForm: React.FC = () => {
               autoFocus
             />
 
-            <Button type="submit" loading={createSoftwareLoading}>
+            <Button type="submit" loading={state.createSoftwareLoading}>
               Salvar
             </Button>
           </form>
