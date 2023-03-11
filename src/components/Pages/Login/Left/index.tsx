b08ff262e5e693652c4f3c7df7b44e9a2a689c7f
Yasmin <yasminsdcastro@gmail.com>
2023-03-10 21:42:35 -0300

    rollback

--- src/components/Pages/Login/Left/index.tsx
+++ src/components/Pages/Login/Left/index.tsx
@@ -1,31 +1,19 @@
 import { Container, Wrapper } from "./styles";
 
 import Image from "next/image";
-import { useState } from "react";
 
 const LeftSection: React.FC = () => {
-  const [imageError, setImageError] = useState(false);
   return (
     <Wrapper>
       <Container>
-        {imageError ? (
-          <img
-            src="/wizard.png"
-            width={1040}
-            height={1040}
-            alt="Uma mistura de robô com mago sorrindo, com seu bicho de estimação"
-          />
-        ) : (
-          <Image
-            src="/wizard.png"
-            quality={100}
-            width={1040}
-            height={1040}
-            priority
-            onError={(e) => setImageError(true)}
-            alt="Uma mistura de robô com mago sorrindo, com seu bicho de estimação"
-          />
-        )}
+        <Image
+          src="/wizard.png"
+          quality={100}
+          width={1040}
+          height={1040}
+          priority
+          alt="Uma mistura de robô com mago sorrindo, com seu bicho de estimação"
+        />
       </Container>
     </Wrapper>
   );
