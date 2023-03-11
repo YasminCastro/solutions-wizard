1ffd675819ac742a4b508136c994fead292dfebf
Yasmin <yasminsdcastro@gmail.com>
2023-03-10 21:52:12 -0300

    refactor: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,28 +6,20 @@ import authMiddleware from "./backend/middleware/authMiddleware";
 // Supports both a single value or an array of matches
 
 export const config = {
-  matcher: ["/((?!api|static|favicon.ico|icons|manifest.json|_next).*)"],
+  matcher: ["/dashboard", "/software", "/signup", "/problem"],
 };
 
 export default async function middleware(req: NextRequest) {
   const token = req.cookies.get("token");
 
-  if (req.nextUrl.pathname !== "/") {
-    if (!token) {
-      return NextResponse.redirect(new URL("/", req.url));
-    }
+  if (!token) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
 
-    const isAuth = await authMiddleware(token.value);
+  const isAuth = await authMiddleware(token.value);
 
-    if (!isAuth) {
-      req.cookies.delete("user");
-      return NextResponse.redirect(new URL("/", req.url));
-    }
-  } else if (token) {
-    const isAuth = await authMiddleware(token.value);
-
-    if (isAuth) {
-      return NextResponse.redirect(new URL("/dashboard", req.url));
-    }
+  if (!isAuth) {
+    req.cookies.delete("user");
+    return NextResponse.redirect(new URL("/", req.url));
   }
 }
