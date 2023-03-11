bd4ce3398a2d837aeca659e1ac9d8e1a9fed4eac
Yasmin <yasminsdcastro@gmail.com>
2023-03-10 21:39:11 -0300

    fix: image load

--- src/components/Pages/Login/Left/index.tsx
+++ src/components/Pages/Login/Left/index.tsx
@@ -1,19 +1,31 @@
 import { Container, Wrapper } from "./styles";
 
 import Image from "next/image";
+import { useState } from "react";
 
 const LeftSection: React.FC = () => {
+  const [imageError, setImageError] = useState(false);
   return (
     <Wrapper>
       <Container>
-        <Image
-          src="/wizard.png"
-          quality={100}
-          width={1040}
-          height={1040}
-          priority
-          alt="Uma mistura de robô com mago sorrindo, com seu bicho de estimação"
-        />
+        {imageError ? (
+          <img
+            src="/wizard.png"
+            width={1040}
+            height={1040}
+            alt="Uma mistura de robô com mago sorrindo, com seu bicho de estimação"
+          />
+        ) : (
+          <Image
+            src="/wizard.png"
+            quality={100}
+            width={1040}
+            height={1040}
+            priority
+            onError={(e) => setImageError(true)}
+            alt="Uma mistura de robô com mago sorrindo, com seu bicho de estimação"
+          />
+        )}
       </Container>
     </Wrapper>
   );
